8337dfe4fc3c66c28b17a90a4213ed23af26613c
JaredDC <jared.dc@outlook.com>
2020-12-30 01:25:41 +0800

    分为1)带控制台,输出日志版本 和 2)不带控制台版本.

--- build.bat
+++ build.bat
@@ -1,13 +1,18 @@
 @echo off
-pyinstaller  -F -w set_wallpaper.py
+rd /s /q Release
+md Release
 
-rd /s /q build
-md build
-move /y dist\set_wallpaper.exe build\set_wallpaper.exe
+pyinstaller  -F set_wallpaper.py
+move /y dist\set_wallpaper.exe Release\set_wallpaper.exe
+
+pyinstaller  -F -w set_wallpaper.py
+move /y dist\set_wallpaper.exe Release\set_wallpaper_noconsole.exe
+
 echo "********************built********************"
 
 ::clean
 echo "********************cleaning temp files********************"
+rd /s /q build
 rd /s /q __pycache__
 del /q set_wallpaper.spec
 rd /s /q dist
