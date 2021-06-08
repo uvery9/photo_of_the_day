47c59803603c42d026de5294ac763bbac61500df
JaredDC <jared.dc@outlook.com>
2021-06-08 18:54:37 +0800

    rejust the step. build.bat:   1) build   2) copy2localpath   3) create lnk
    cp_lnk_to_startup_dir.bat:
      1) copy lnk to startup_dir

--- build.bat
+++ build.bat
@@ -17,6 +17,49 @@ rd /s /q __pycache__
 del /q photo_of_the_day.spec
 rd /s /q dist
 
-echo "********************succeed********************"
+echo "************ copy to LOCALAPPDATA ************"
 
-pause
+set EXE_DIR=%LOCALAPPDATA%\photo_of_the_day
+if exist "%EXE_DIR%" (		
+		echo directory %EXE_DIR% exists
+	) else (
+		echo make %EXE_DIR%
+		%HOMEDRIVE%
+		md %EXE_DIR%
+	)
+cd /d %~dp0
+xcopy Release\photo_of_the_day.exe "%EXE_DIR%" /y /i /q /d
+
+
+echo "************ create lnk ************"
+set EXE_DIR=%LOCALAPPDATA%\photo_of_the_day
+set INK_FILE=photo_of_the_day.lnk
+
+echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
+echo sLinkFile = "D:\jared\coding\photo_of_the_day\photo_of_the_day.lnk" >> CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
+echo oLink.TargetPath = "%EXE_DIR%\photo_of_the_day.exe" >> CreateShortcut.vbs
+REM echo oLink.TargetPath = "C:\Users\jared\AppData\Local\photo_of_the_day\photo_of_the_day.exe" >> CreateShortcut.vbs
+echo oLink.Save >> CreateShortcut.vbs
+
+if exist "%INK_FILE%" (		
+		echo file %INK_FILE% exists
+	) else (
+		cscript CreateShortcut.vbs
+	)	
+del CreateShortcut.vbs
+
+REM https://superuser.com/questions/392061/how-to-make-a-shortcut-from-cmd
+REM Set oWS = WScript.CreateObject("WScript.Shell")
+REM sLinkFile = "C:\MyShortcut.LNK"
+REM Set oLink = oWS.CreateShortcut(sLinkFile)
+REM     oLink.TargetPath = "C:\Program Files\MyApp\MyProgram.EXE"
+REM  '  oLink.Arguments = ""
+REM  '  oLink.Description = "MyProgram"   
+REM  '  oLink.HotKey = "ALT+CTRL+F"
+REM  '  oLink.IconLocation = "C:\Program Files\MyApp\MyProgram.EXE, 2"
+REM  '  oLink.WindowStyle = "1"   
+REM  '  oLink.WorkingDirectory = "C:\Program Files\MyApp"
+REM oLink.Save
+echo "********************succeed********************"
+ping -n 5 127.0.0.1 > nul
--- cp_lnk_to_startup_dir.bat
+++ cp_lnk_to_startup_dir.bat
@@ -15,49 +15,16 @@ if %errorlevel%==0 ( rd "%SystemRoot%\system32\%uac%" >nul 2>nul ) else (
 	del /f /q "%temp%\%uac%.vbs" & exit )
 endlocal
 
-set EXE_DIR=%LOCALAPPDATA%\photo_of_the_day
-if exist "%EXE_DIR%" (		
-		echo directory %EXE_DIR% exists
-	) else (
-		echo make %EXE_DIR%
-		%HOMEDRIVE%
-		md %EXE_DIR%
-	)
-cd /d %~dp0
-xcopy Release\photo_of_the_day.exe "%EXE_DIR%" /y /i /q /d
-del "%HOMEDRIVE%%HOMEPATH%\Desktop\photo_of_the_day.lnk"
+:: copy lnk to startup directory.
 
-echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-echo sLinkFile = "%HOMEDRIVE%%HOMEPATH%\Desktop\photo_of_the_day.lnk" >> CreateShortcut.vbs
-echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
-echo oLink.TargetPath = "%EXE_DIR%\photo_of_the_day.exe" >> CreateShortcut.vbs
-REM echo oLink.TargetPath = "C:\Users\jared\AppData\Local\photo_of_the_day\photo_of_the_day.exe" >> CreateShortcut.vbs
-echo oLink.Save >> CreateShortcut.vbs
-cscript CreateShortcut.vbs
-del CreateShortcut.vbs
-
-REM https://superuser.com/questions/392061/how-to-make-a-shortcut-from-cmd
-REM Set oWS = WScript.CreateObject("WScript.Shell")
-REM sLinkFile = "C:\MyShortcut.LNK"
-REM Set oLink = oWS.CreateShortcut(sLinkFile)
-REM     oLink.TargetPath = "C:\Program Files\MyApp\MyProgram.EXE"
-REM  '  oLink.Arguments = ""
-REM  '  oLink.Description = "MyProgram"   
-REM  '  oLink.HotKey = "ALT+CTRL+F"
-REM  '  oLink.IconLocation = "C:\Program Files\MyApp\MyProgram.EXE, 2"
-REM  '  oLink.WindowStyle = "1"   
-REM  '  oLink.WorkingDirectory = "C:\Program Files\MyApp"
-REM oLink.Save
-
-:: copy vbs to startup directory.
-
-set INK_PATH=%HOMEDRIVE%%HOMEPATH%\Desktop\photo_of_the_day.lnk
-set LNK_DEST=C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\photo_of_the_day.lnk
+set INK_NAME=photo_of_the_day.lnk
+set LNK_DEST=C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\%INK_NAME%
 set EXE_DIR=%LOCALAPPDATA%\photo_of_the_day
 if exist "%LNK_DEST%" (		
 		echo file %LNK_DEST% exists
 	) else (
-		copy %INK_PATH%  "%LNK_DEST%" /y
+		cd /d %~dp0
+		copy %INK_NAME%  "%LNK_DEST%" /y
 	)
 
 ping -n 5 127.0.0.1 > nul
